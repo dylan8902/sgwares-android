7e267e33a95dd7a5826669aa2bec762250cbcb59
Dylan Jones <dyl@anjon.es>
2017-06-04 22:43:49 +0100

    Add ability to update name (#1) and colour (#2) from settings fragment

--- app/src/main/java/com/sgwares/android/fragments/SettingsFragment.java
+++ app/src/main/java/com/sgwares/android/fragments/SettingsFragment.java
@@ -1,21 +1,86 @@
 package com.sgwares.android.fragments;
 
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sgwares.android.R;
+import com.sgwares.android.models.User;
 
-/**
- * Created by dylan8902 on 06/05/2017.
- */
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-public class SettingsFragment extends PreferenceFragment {
+    private static final String TAG = "SettingsFragment";
+    private static final String PREF_KEY_NAME = "pref_key_name";
+    private static final String PREF_KEY_COLOUR = "pref_key_colour";
+    private DatabaseReference mUserRef;
+    private User mUser;
+    private EditTextPreference mNamePref;
+    private EditTextPreference mColourPref;
+    private ValueEventListener mUserListener;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings);
+
+        mNamePref = (EditTextPreference) findPreference(PREF_KEY_NAME);
+        mColourPref = (EditTextPreference) findPreference(PREF_KEY_COLOUR);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+        mUserRef = usersRef.child(auth.getCurrentUser().getUid());
+        mUserListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: " + dataSnapshot.toString());
+                mUser = dataSnapshot.getValue(User.class);
+                mNamePref.setText(mUser.getName());
+                mColourPref.setText(mUser.getColour());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: " + databaseError.getMessage());
+            }
+        };
+        mUserRef.addValueEventListener(mUserListener);
     }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.d(TAG, "onSharedPreferenceChanged: " + key);
+
+        if (PREF_KEY_NAME.equals(key)) {
+            mUser.setName(mNamePref.getEditText().getText().toString());
+            mUserRef.setValue(mUser);
+        } else if (PREF_KEY_COLOUR.equals(key)) {
+            mUser.setColour(mColourPref.getEditText().getText().toString());
+            mUserRef.setValue(mUser);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        if ((mUserRef != null) && (mUserListener != null)) {
+            mUserRef.removeEventListener(mUserListener);
+        }
+        super.onPause();
+    }
+
 }
